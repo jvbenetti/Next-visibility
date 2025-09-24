db888384f822a74c04f3d7d7bf07291d36ee9470
JaoVicy <filipimjoa@gmail.com>
2025-09-24 20:00:07 -0300

    build: added global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <style>{`
+                * {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                }
+            `}
+            </style>
             Esse texto aparece em todas as páginas!
             <Component {...pageProps} />
         </>
